7c2b33e83324925b9e4d532b0570e82e3ba65022
Brukvo <smullerin@gmail.com>
2021-01-25 22:40:14 +0900

    задание 2 к уроку №7

--- lesson07/task02.py
+++ lesson07/task02.py
@@ -13,4 +13,65 @@
 этом уроке знания: реализовать абстрактные классы для основных классов проекта,
 проверить на практике работу декоратора @property.
 """
-from abc import ABC
+from abc import ABC, abstractmethod
+
+
+class Clothes(ABC):
+    fabric_c9n = []
+
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    @abstractmethod
+    def calc_consumption(self):
+        pass
+
+    @staticmethod
+    def calc_total_c9n():
+        return sum(Clothes.fabric_c9n)
+
+
+class Coat(Clothes):
+
+    def __init__(self, name, size):
+        self.v = size
+        super(Coat, self).__init__(name)
+
+    def calc_consumption(self):
+        # V/6.5 + 0.5
+        result = round(self.v / 6.5 + .5, 2)
+        Clothes.fabric_c9n.append(result)
+        return result
+
+
+class Costume(Clothes):
+
+    def __init__(self, name, height):
+        self.h = height
+        super(Costume, self).__init__(name)
+
+    def calc_consumption(self):
+        # 2*H + 0.3
+        result = round(2 * self.h + .3, 2)
+        Clothes.fabric_c9n.append(result)
+        return result
+
+
+c1 = Costume('Заря', 56)
+c1_cons = c1.calc_consumption()
+print('Костюм "{}", рост {}, расход ткани составил {} единиц'.format(c1.name, c1.h, c1_cons))
+
+c2 = Coat('Крестьянка', 108)
+c2_cons = c2.calc_consumption()
+print('Пальто "{}", размер {}, расход ткани составил {} единиц'.format(c2.name, c2.v, c2_cons))
+
+print('Итоговый расход ткани:', round(Clothes.calc_total_c9n(), 2), 'единиц')
+
